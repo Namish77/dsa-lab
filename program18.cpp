84ffda5c195acb956d32e04026582dc6307a15b8
Namish77 <namish150877@gmail.com>
2025-11-23 17:30:56 +0530

    program18.cpp

--- program18.cpp
+++ program18.cpp
@@ -4,6 +4,68 @@
 #include <iostream>
 using namespace std;
 
-struct Node{ int v; Node* next; Node(int x):v(x),next(NULL){} };
+struct Node {
+    int v;
+    Node* next;
+    Node(int x): v(x), next(nullptr) {}
+};
 
-int main(){ return 0; }
+class CList {
+    Node* last;
+
+public:
+    CList(): last(nullptr) {}
+
+    void insertEnd(int x) {
+        Node* n = new Node(x);
+        if (!last) {
+            last = n;
+            n->next = n;
+        } else {
+            n->next = last->next;
+            last->next = n;
+            last = n;
+        }
+        cout << x << " inserted\n";
+    }
+
+    void delFront() {
+        if (!last) {
+            cout << "Empty\n";
+            return;
+        }
+        Node* f = last->next;
+        if (f == last) {
+            last = nullptr;
+        } else {
+            last->next = f->next;
+        }
+        cout << f->v << " deleted\n";
+        delete f;
+    }
+
+    void display() {
+        if (!last) {
+            cout << "Empty\n";
+            return;
+        }
+        cout << "CList: ";
+        Node* t = last->next;
+        do {
+            cout << t->v << " ";
+            t = t->next;
+        } while (t != last->next);
+        cout << "\n";
+    }
+};
+
+int main() {
+    CList C;
+    C.insertEnd(10);
+    C.insertEnd(20);
+    C.insertEnd(30);
+    C.display();
+    C.delFront();
+    C.display();
+    return 0;
+}
