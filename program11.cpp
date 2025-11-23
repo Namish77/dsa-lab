a0cebe4d0fd608ce549f223df5fee7ae7faa18b9
Namish77 <namish150877@gmail.com>
2025-11-23 17:34:57 +0530

    program11.cpp

--- program11.cpp
+++ program11.cpp
@@ -5,11 +5,66 @@
 using namespace std;
 
 class ArrQueue {
-    int Q[6], f, r;
+    int Q[6];
+    int f, r;
+
 public:
-    ArrQueue():f(0),r(-1){}
-    void enqueue(int x){ if(r<5) Q[++r]=x; }
-    void dequeue(){ if(f<=r) f++; }
+    ArrQueue() : f(0), r(-1) {}
+
+    void enqueue(int x) {
+        if (r == 5) {
+            cout << "Q Overflow\n";
+            return;
+        }
+        Q[++r] = x;
+        cout << x << " enqueued\n";
+    }
+
+    void dequeue() {
+        if (f > r) {
+            cout << "Q Underflow\n";
+            return;
+        }
+        cout << Q[f++] << " dequeued\n";
+    }
+
+    void peek() {
+        if (f > r)
+            cout << "Empty\n";
+        else
+            cout << "Front: " << Q[f] << "\n";
+    }
+
+    void display() {
+        if (f > r)
+            cout << "Empty\n";
+        else {
+            cout << "Queue: ";
+            for (int i = f; i <= r; i++) cout << Q[i] << " ";
+            cout << "\n";
+        }
+    }
 };
 
-int main(){ ArrQueue q; q.enqueue(1); q.dequeue(); }
+int main() {
+    ArrQueue q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+    q.enqueue(6);
+
+    q.display();
+    q.peek();
+    q.dequeue();
+    q.dequeue();
+    q.display();
+
+    q.dequeue();
+    q.dequeue();
+    q.dequeue();
+    q.dequeue();
+
+    return 0;
+}
