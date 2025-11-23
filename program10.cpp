fcdb7fa87498ebb01f82b647c1e0a8aff5e4400d
Namish77 <namish150877@gmail.com>
2025-11-23 17:35:26 +0530

    program10.cpp

--- program10.cpp
+++ program10.cpp
@@ -4,14 +4,54 @@
 #include <iostream>
 using namespace std;
 
-struct Node{ int v; Node* next; Node(int x):v(x),next(NULL){} };
-
-class LStack{
-    Node* top;
-public:
-    LStack():top(NULL){}
-    void push(int x){ Node* n=new Node(x); n->next=top; top=n; }
-    void pop(){ if(top){ Node* t=top; top=top->next; delete t; } }
+struct Node {
+    int val;
+    Node* next;
+    Node(int v) : val(v), next(nullptr) {}
 };
 
-int main(){ LStack s; s.push(10); s.pop(); }
+class LStack {
+    Node* top;
+
+public:
+    LStack() : top(nullptr) {}
+
+    void push(int v) {
+        Node* n = new Node(v);
+        n->next = top;
+        top = n;
+        cout << v << " pushed\n";
+    }
+
+    void pop() {
+        if (!top) {
+            cout << "Empty\n";
+            return;
+        }
+        Node* t = top;
+        cout << t->val << " popped\n";
+        top = top->next;
+        delete t;
+    }
+
+    void display() {
+        Node* cur = top;
+        cout << "Stack: ";
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->next;
+        }
+        cout << "\n";
+    }
+};
+
+int main() {
+    LStack s;
+    s.push(5);
+    s.push(10);
+    s.push(15);
+    s.display();
+    s.pop();
+    s.display();
+    return 0;
+}
