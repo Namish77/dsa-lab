a27fdd308bb4d68ceada4328fc2d96ced0e0e052
Namish77 <namish150877@gmail.com>
2025-11-23 18:43:51 +0530

    program18.cpp

--- program18.cpp
+++ program18.cpp
@@ -1,71 +1,45 @@
-// Program 18 - Circular Linked List
-// Author: Namish (Roll 241478)
-
 #include <iostream>
 using namespace std;
 
-struct Node {
-    int v;
-    Node* next;
-    Node(int x): v(x), next(nullptr) {}
-};
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
 
-class CList {
-    Node* last;
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
 
-public:
-    CList(): last(nullptr) {}
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
 
-    void insertEnd(int x) {
-        Node* n = new Node(x);
-        if (!last) {
-            last = n;
-            n->next = n;
-        } else {
-            n->next = last->next;
-            last->next = n;
-            last = n;
-        }
-        cout << x << " inserted\n";
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, n, largest);
     }
+}
 
-    void delFront() {
-        if (!last) {
-            cout << "Empty\n";
-            return;
-        }
-        Node* f = last->next;
-        if (f == last) {
-            last = nullptr;
-        } else {
-            last->next = f->next;
-        }
-        cout << f->v << " deleted\n";
-        delete f;
-    }
-
-    void display() {
-        if (!last) {
-            cout << "Empty\n";
-            return;
-        }
-        cout << "CList: ";
-        Node* t = last->next;
-        do {
-            cout << t->v << " ";
-            t = t->next;
-        } while (t != last->next);
-        cout << "\n";
-    }
-};
+void buildMaxHeap(int arr[], int n) {
+    for (int i = n/2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+}
 
 int main() {
-    CList C;
-    C.insertEnd(10);
-    C.insertEnd(20);
-    C.insertEnd(30);
-    C.display();
-    C.delFront();
-    C.display();
+    int n;
+    cin >> n;
+
+    int arr[100];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    buildMaxHeap(arr, n);
+
+    cout << "Max Heap: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    cout << endl;
     return 0;
 }
