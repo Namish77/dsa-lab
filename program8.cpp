54d70b67e12fb1a556a2040a57bd73068ff1ace9
Namish77 <namish150877@gmail.com>
2025-11-23 17:36:26 +0530

    program8.cpp

--- program8.cpp
+++ program8.cpp
@@ -4,8 +4,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int base,size,rows,cols,i,j;
-    cin>>base>>size>>rows>>cols>>i>>j;
-    cout<< base + ((i*cols)+j)*size;
+int main() {
+    int base, size, rows, cols, i, j;
+    cout << "Base address: "; 
+    cin >> base;
+
+    cout << "Element size (bytes): "; 
+    cin >> size;
+
+    cout << "Rows: "; cin >> rows;
+    cout << "Cols: "; cin >> cols;
+
+    cout << "Index i j (0-based): "; 
+    cin >> i >> j;
+
+    int rowMajor = base + ((i*cols) + j) * size;
+    int colMajor = base + ((j*rows) + i) * size;
+
+    cout << "Row-major addr: " << rowMajor << "\n";
+    cout << "Column-major addr: " << colMajor << "\n";
+
+    return 0;
 }
