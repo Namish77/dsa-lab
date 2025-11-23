4d2fe0a268467e775d458b56c73e60b241112917
Namish77 <namish150877@gmail.com>
2025-11-23 17:34:29 +0530

    program12.cpp

--- program12.cpp
+++ program12.cpp
@@ -4,20 +4,69 @@
 #include <iostream>
 using namespace std;
 
-struct Node{ int v; Node* next; Node(int x):v(x),next(NULL){} };
+struct Node {
+    int v;
+    Node* next;
+    Node(int x): v(x), next(nullptr) {}
+};
 
-class LQueue{
-    Node* f; Node* r;
+class LQueue {
+    Node* f;
+    Node* r;
+
 public:
-    LQueue():f(NULL),r(NULL){}
-    void enqueue(int x){
-        Node* n=new Node(x);
-        if(!r) f=r=n;
-        else r->next=n, r=n;
+    LQueue() : f(nullptr), r(nullptr) {}
+
+    void enqueue(int x) {
+        Node* n = new Node(x);
+        if (!r) {
+            f = r = n;
+        } else {
+            r->next = n;
+            r = n;
+        }
+        cout << x << " enqueued\n";
     }
-    void dequeue(){
-        if(f){ Node* t=f; f=f->next; delete t; if(!f) r=NULL; }
+
+    void dequeue() {
+        if (!f) {
+            cout << "Empty\n";
+            return;
+        }
+        Node* t = f;
+        cout << t->v << " dequeued\n";
+        f = f->next;
+        delete t;
+        if (!f) r = nullptr;
+    }
+
+    void display() {
+        Node* cur = f;
+        cout << "Queue: ";
+        while (cur) {
+            cout << cur->v << " ";
+            cur = cur->next;
+        }
+        cout << "\n";
     }
 };
 
-int main(){ LQueue q; q.enqueue(5); q.dequeue(); }
+int main() {
+    LQueue q;
+
+    q.enqueue(7);
+    q.enqueue(8);
+    q.enqueue(9);
+    q.display();
+
+    q.dequeue();
+    q.display();
+
+    while (true) {
+        if (!q.f) break;
+        q.dequeue();
+    }
+
+    q.dequeue();
+    return 0;
+}
