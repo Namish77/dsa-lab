b225f6959fd69a6b57d58be545fa85533262ba51
Namish77 <namish150877@gmail.com>
2025-11-23 17:38:08 +0530

    Update program5.cpp

--- program5.cpp
+++ program5.cpp
@@ -6,12 +6,25 @@ using namespace std;
 
 int main() {
     int a[100], n;
-    cin >> n;
+    cout << "N: "; cin >> n;
+    cout << "Elements:\n";
     for (int i=0;i<n;i++) cin >> a[i];
 
-    for (int p=0;p<n-1;p++)
-        for (int j=0;j<n-p-1;j++)
-            if(a[j]>a[j+1]) swap(a[j],a[j+1]);
+    for (int p=0;p<n-1;p++) {
+        cout << "Pass " << p+1 << ": ";
+        for (int j=0;j<n-p-1;j++) {
+            if (a[j] > a[j+1]) {
+                int t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+        }
+        for (int z=0;z<n;z++) cout << a[z] << " ";
+        cout << "\n";
+    }
 
+    cout << "Sorted: ";
     for (int i=0;i<n;i++) cout << a[i] << " ";
+    cout << endl;
+    return 0;
 }
