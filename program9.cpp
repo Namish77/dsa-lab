b02b167c97bc867b2d43b3cae0c3f3754ed3b256
Namish77 <namish150877@gmail.com>
2025-11-23 17:35:58 +0530

    program9.cpp

--- program9.cpp
+++ program9.cpp
@@ -5,11 +5,54 @@
 using namespace std;
 
 class ArrStack {
-    int S[6], top;
+    int S[6];
+    int top;
+    const int cap = 6;
+
 public:
-    ArrStack(): top(-1){}
-    void push(int x){ if(top<5) S[++top]=x; }
-    void pop(){ if(top>=0) top--; }
+    ArrStack() : top(-1) {}
+
+    void push(int x) {
+        if (top == cap-1) {
+            cout << "Overflow: " << x << "\n";
+            return;
+        }
+        S[++top] = x;
+    }
+
+    void pop() {
+        if (top == -1) {
+            cout << "Underflow\n";
+            return;
+        }
+        cout << S[top--] << " popped\n";
+    }
+
+    int peek() {
+        if (top == -1) {
+            cout << "Empty\n";
+            return -1;
+        }
+        return S[top];
+    }
+
+    bool empty() {
+        return top == -1;
+    }
 };
 
-int main(){ ArrStack s; s.push(1); s.pop(); }
+int main() {
+    ArrStack st;
+    st.push(11);
+    st.push(22);
+    st.push(33);
+    st.push(44);
+    st.push(55);
+    st.push(66); // ok
+    st.push(77); // overflow
+
+    while (!st.empty()) st.pop();
+    st.pop(); // underflow
+
+    return 0;
+}
