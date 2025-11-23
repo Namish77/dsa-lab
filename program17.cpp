64ae8657d548c85741150b0df9d698786ac5f730
Namish77 <namish150877@gmail.com>
2025-11-23 17:31:31 +0530

    program17.cpp

--- program17.cpp
+++ program17.cpp
@@ -4,9 +4,68 @@
 #include <iostream>
 using namespace std;
 
-struct Poly{
-    int c,p; Poly* next;
-    Poly(int a,int b):c(a),p(b),next(NULL){}
+struct Poly {
+    int coef, pow;
+    Poly* next;
+    Poly(int c, int p): coef(c), pow(p), next(nullptr) {}
 };
 
-int main(){ return 0; }
+void insertTerm(Poly*& H, int c, int p) {
+    Poly* n = new Poly(c,p);
+    if (!H) H = n;
+    else {
+        Poly* t = H;
+        while (t->next) t = t->next;
+        t->next = n;
+    }
+}
+
+Poly* addPoly(Poly* A, Poly* B) {
+    Poly* R = nullptr;
+    while (A && B) {
+        if (A->pow == B->pow) {
+            insertTerm(R, A->coef + B->coef, A->pow);
+            A = A->next; B = B->next;
+        } else if (A->pow > B->pow) {
+            insertTerm(R, A->coef, A->pow);
+            A = A->next;
+        } else {
+            insertTerm(R, B->coef, B->pow);
+            B = B->next;
+        }
+    }
+    while (A) { insertTerm(R, A->coef, A->pow); A = A->next; }
+    while (B) { insertTerm(R, B->coef, B->pow); B = B->next; }
+    return R;
+}
+
+void printPoly(Poly* P) {
+    while (P) {
+        cout << P->coef << "x^" << P->pow;
+        if (P->next) cout << " + ";
+        P = P->next;
+    }
+    cout << "\n";
+}
+
+int main() {
+    Poly *A = nullptr, *B = nullptr;
+
+    insertTerm(A, 3, 3);
+    insertTerm(A, 2, 2);
+    insertTerm(A, 1, 0);
+
+    insertTerm(B, 4, 3);
+    insertTerm(B, 2, 1);
+    insertTerm(B, 5, 0);
+
+    cout << "A: "; printPoly(A);
+    cout << "B: "; printPoly(B);
+
+    Poly* R = addPoly(A,B);
+
+    cout << "Sum: ";
+    printPoly(R);
+
+    return 0;
+}
